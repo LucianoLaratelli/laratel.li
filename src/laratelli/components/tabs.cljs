ee4149a38ed1801c1b2cb0c7c0b9f9131b38f087
Luciano Laratelli <luciano@laratel.li>
2022-09-06 22:08:17 -0400

    small refactor of tabs component

--- src/laratelli/components/tabs.cljs
+++ src/laratelli/components/tabs.cljs
@@ -5,22 +5,22 @@
 
 (defn one-tab
   [tab active]
-  (let [class (if (= active :active) "drac-tab drac-tab-active" "drac-tab")]
+  (let [class (if (= active :active)
+                "drac-tab drac-tab-active"
+                "drac-tab")]
     (vector :li {:class class}
             [:> drac/Anchor {:class "drac-tab-link drac-text"
                              :href (rfe/href (get routes/refs (keyword tab)))}
              tab])))
 
 (defn make-tabs [active-tab]
-  (let [tabs ["home"
-              "contact" "cv" "posts"]
-        tab-colors {"home" "purple"
-                    "contact" "yellow"
-                    "cv" "green"
-                    "posts" "cyan"}]
+  (let [tab-names ["home" "contact" "cv" "posts"]
+        tab-colors ["purple" "yellow" "green" "cyan"]
+        _ (assert (= (count tab-names) (count tab-colors)))
+        tabs-with-color (apply assoc {} (interleave tab-names tab-colors))]
     (into [:> drac/Tabs
-           {:color (get tab-colors active-tab)}]
+           {:color (get tabs-with-color active-tab)}]
           (map #(one-tab % (if (= % active-tab)
                              :active
                              :inactive))
-               tabs))))
+               tab-names))))
